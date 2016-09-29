5a6f1f18e55244eb25d4d0f01bd5cb9b523f4652
Ivan Maria Izquierdo <iv.ivan.maria@gmail.com>
2016-09-29 22:48:51 +0200

    0.0.1 Release

---
+++ README.md
@@ -0,0 +1,23 @@
+doxygen
+===========
+
+Node plugin for building [Doxygen](www.doxygen.org) like documentation.
+
+##Setup
+
+`npm install doxygen`
+
+##About this module
+
+This module is in its first stages and lacking important features.
+
+  - Linux support
+  - Mac support
+  - Automated tests and builds
+  - Improve and test the support for older versions
+
+As it stands, only the following is implemented
+
+  - Windows support
+  - Working with latests(1.8.12) versions of doxygen
+  - Generate and use custom config files with a set of options
---
+++ index.js
@@ -0,0 +1,26 @@
+ 'use strict';
+
+var installModule = require('./src/installVersion');
+var configModule = require('./src/createConfig');
+
+var exec = require('child_process').execSync;
+var extend = require('extend');
+var defaultOptions = {
+    version: "1.8.12"
+}
+function run(userOptions){
+  var options = extend(defaultOptions, userOptions);
+
+  if (!options.configPath){
+    throw "A valid doxygen config file must be provided via the configPath property"
+  } else {
+    var dirName = __dirname;
+    exec('"' + dirName + '/dist/' + options.version + '/doxygen" "' + options.configPath + '"');
+  }
+}
+
+module.exports = {
+  run: run,
+  installVersion: installModule.installVersion,
+  createConfig: configModule.createConfig
+};
