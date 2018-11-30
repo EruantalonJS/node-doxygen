3c78e9df24d119b45b692dea9149f1a2011bb6b5
ES\imaria <imaria@deloitte.es>
2018-11-30 19:08:55 +0100

    Code climate fixes

--- bin/nodeDoxygen.js
+++ bin/nodeDoxygen.js
@@ -10,53 +10,60 @@ var params;
 var version;
 
 for (var i = 0; i < doxygenParams.length; i++) {
-    if (doxygenParams[i].startsWith("--jsonParams=")) {
-        params = JSON.parse(doxygenParams[i].substring(13));
-    }
-    else if (doxygenParams[i].startsWith("--configPath=")) {
-        configPath = doxygenParams[i].substring(13);
-    }
-    else if (doxygenParams[i].startsWith("--version=")) {
-        version = doxygenParams[i].substring(10);
-    }
-    else if (doxygenParams[i] == "--config") {
-        if (operationSet) {
-            console.warn("Option --config ignored: Only one command can be executed at the same time");
-        }
-        else {
-            operationSet = true;
-            operation = "config";
-        }
+    var currentParam = doxygenParams[i];
+    var currentParamEqualIndex = currentParam.indexOf("=");
+    var currentOption;
 
-    } else if (doxygenParams[i] == "--download") {
-        if (operationSet) {
-            console.warn("Option --download ignored: Only one command can be executed at the same time");
-        }
-        else {
-            operationSet = true;
-            operation = "download";
-        }
-    } else if (doxygenParams[i] == "--docs") {
-        if (operationSet) {
-            console.warn("Option --docs ignored: Only one command can be executed at the same time");
-        }
-        else {
-            operationSet = true;
-            operation = "docs";
-        }
+    if (currentParamEqualIndex  !== -1){
+        currentOption = currentParam;
+    } else {
+        currentOption = currentParam.substring(0, currentParamEqualIndex - 1);
+    }
+
+    switch(currentOption){
+        case "--jsonParams":
+            params = JSON.parse(currentParam.substring(currentParamEqualIndex + 1));
+            break;
+        case "--configPath":
+            configPath = currentParam.substring(currentParamEqualIndex + 1);
+            break;
+        case "--version":
+            version = currentParam.substring(currentParamEqualIndex + 1);
+            break;
+        case "--config":
+            selectOperation("config");
+            break;
+        case "--download":
+            selectOperation("download");
+            break;
+        case "--docs":
+            selectOperation("docs");
+            break;
     }
 }
 
 switch (operation) {
-    case ("docs"):
+    case "docs":
         doxygen.run(configPath, version);
         break;
 
-    case ("config"):
+    case "config":
         doxygen.createConfig(params, configPath);
         break;
 
-    case ("download"):
+    case "download":
         doxygen.downloadVersion(version);
         break;
+}
+
+function selectOperation(operationName){
+    if (operationSet) {
+        console.warn("Option --" + 
+                    operationName + 
+                    " ignored: Only one command can be executed at the same time");
+    }
+    else {
+        operationSet = true;
+        operation = "operationName";
+    }
 }
